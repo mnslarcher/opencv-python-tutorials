4ec62911e268f17f1564470340ea75b20ad66fb4
Mario Namtao Shianti Larcher <mnslarcher@gmail.com>
2022-08-29 08:33:20 +0200

    Add 2 samples, update env and add geo transf

--- 03-image-processing-in-opencv/02-geometric-transformations-of-images.py
+++ 03-image-processing-in-opencv/02-geometric-transformations-of-images.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import matplotlib.pyplot as plt
 import numpy as np
 
 # Scaling
@@ -33,5 +34,86 @@ cv.destroyAllWindows()
 
 
 # Rotation
+
+# Rotation by a theta angle:
+# [[cosO, -sinO], 
+#  [sinO, cosO]]
+
+# Scaled rotation with adjustable center of rotation
+# [[a,  b, (1 - a) * center_x - b * center_y],
+#  [-b, a, b * center_x + (1 - a) * center_y]]
+
+img = cv.imread("../samples/data/messi5.jpg", 0)
+rows, cols = img.shape
+
+# cols - 1 and rows - 1 are the coordinates limits
+# center, angle, scale
+M = cv.getRotationMatrix2D(((cols - 1) / 2.0, (rows - 1) / 2.0), 90, 1)
+dst = cv.warpAffine(img, M, (cols, rows))
+
+cv.imshow("image", cv.hconcat([img, dst]))
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+
+# Affine transformation
+
+# [[A,   t], 
+#  [0^T, 1]
+# Invariance: parallelism, volume ratio
+
+img = cv.imread("../samples/data/sudoku.png")
+rows, cols, ch = img.shape
+
+pts1 = np.float32([[50, 50], [200, 50], [50, 200]])
+pts2 = np.float32([[10, 100], [200, 50], [100, 250]])
+
+for pt in pts1:
+    img = cv.circle(img, pt.astype(int), 5, color=(0, 255, 0), thickness=-1)
+
+M = cv.getAffineTransform(pts1, pts2)
+dst = cv.warpAffine(img, M, (cols, rows))
+
+plt.figure("Affine transformation")
+plt.subplot(121)
+plt.imshow(img)
+plt.title("Input")
+
+plt.subplot(122)
+plt.imshow(dst)
+plt.title("Ouput")
+
+plt.show()
+
+
+# Perspective Transformation
+
+# [[A,   t], 
+#  [a^T, v]
+# Invariance: plane intersection and tangency
+
+img = cv.imread("../samples/data/sudoku.png")
+rows, cols, ch = img.shape
+
+pts1 = np.float32([[56, 65], [368, 52], [28, 387], [389, 390]])
+pts2 = np.float32([[0, 0], [300, 0], [0, 300], [300, 300]])
+
+for pt in pts1:
+    img = cv.circle(img, pt.astype(int), 5, color=(0, 255, 0), thickness=-1)
+
+M = cv.getPerspectiveTransform(pts1, pts2)
+dst = cv.warpPerspective(img, M, (cols, rows))
+
+plt.figure("Perspective Transformation")
+plt.subplot(121)
+plt.imshow(img)
+plt.title("Input")
+
+plt.subplot(122)
+plt.imshow(dst)
+plt.title("Ouput")
+
+plt.show()
+
 
 
